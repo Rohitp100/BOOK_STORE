6c3362368d83d3fce85b8bfe58fd4e5d9fd8c66a
Rohitp100 <106956998+Rohitp100@users.noreply.github.com>
2024-02-26 13:37:55 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,5 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://root:KW59ROkcZJOwGJSm@books-store-mern.qvxnc1n.mongodb.net/books-collection?retryWrites=true&w=majority'
+export const mongoDBURL = ''
+
+// Please create a free database for yourself.
